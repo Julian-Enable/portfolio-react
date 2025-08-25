1e4c996449935d2655c0f32d21524ce42386be78
Julian Merchan <elabc80@gmail.com>
2025-08-25 16:04:51 -0500

    Add Spotify Profile project and image
    Added a new project entry for 'Spotify Profile' to mywork_data.js and included its associated SVG image (project_8.svg) in the assets.

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -5,6 +5,7 @@ import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
 import project7_img from '../assets/project_7.svg'
+import project8_img from '../assets/project_8.svg'
 
 const mywork_data = [
     {
@@ -48,6 +49,12 @@ const mywork_data = [
         w_name: "CancionEmocion",
         w_img: project7_img,
         w_link: "https://cancionemocion.netlify.app/"
+    },
+    {
+        w_no: 8,
+        w_name: "Spotify Profile",
+        w_img: project8_img,
+        w_link: "https://github.com/Julian-Enable/spotify-profile"
     },
 ]
 
